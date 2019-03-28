d8cc0412ef4d62b7e8d2b26d289294b611a97840
Curro Márquez <curro171093@gmail.com>
2019-03-28 18:34:39 +0100

    Fixed bugs
    Bugs fixed

--- src/main/java/controllers/RouteController.java
+++ src/main/java/controllers/RouteController.java
@@ -2,7 +2,6 @@
 package controllers;
 
 import java.util.Collection;
-import java.util.Date;
 
 import javax.validation.Valid;
 
@@ -10,19 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import security.LoginService;
-import security.UserAccount;
 import services.ActorService;
 import services.RouteService;
 import domain.Driver;
 import domain.Finder;
-import domain.Passenger;
 import domain.Route;
 
 @Controller
@@ -30,10 +25,12 @@ import domain.Route;
 public class RouteController extends AbstractController {
 
 	// Services ---------------------------------------------------------------
-	@Autowired
-	private ActorService	actorService;
+
 	@Autowired
 	private RouteService	routeService;
+
+	@Autowired
+	private ActorService	actorService;
 
 
 	// Constructors -----------------------------------------------------------
@@ -57,47 +54,7 @@ public class RouteController extends AbstractController {
 
 	}
 
-	@RequestMapping(value = "/driver/listActive", method = RequestMethod.GET)
-	public ModelAndView listActiveRoutesByDriver() {
-		ModelAndView result;
-		Collection<Route> routes;
-		Driver driver;
-		UserAccount ua;
-
-		ua = LoginService.getPrincipal();
-		driver = (Driver) this.actorService.findByUserAccount(ua);
-		Assert.notNull(driver);
-
-		routes = this.routeService.findActiveRoutesByDriver(driver.getId(), new Date());
-
-		result = new ModelAndView("route/list");
-		result.addObject("routes", routes);
-		result.addObject("driver", driver);
-		result.addObject("requestURI", "route/driver/listActive.do");
-
-		return result;
-	}
-
-	@RequestMapping(value = "/passenger/listActive", method = RequestMethod.GET)
-	public ModelAndView listActiveRoutesByPassenger() {
-		ModelAndView result;
-		Collection<Route> routes;
-		Passenger passenger;
-		UserAccount ua;
-
-		ua = LoginService.getPrincipal();
-		passenger = (Passenger) this.actorService.findByUserAccount(ua);
-		Assert.notNull(passenger);
-
-		routes = this.routeService.findActiveRoutesByPassenger(passenger.getId());
-
-		result = new ModelAndView("route/list");
-		result.addObject("routes", routes);
-		result.addObject("passenger", passenger);
-		result.addObject("requestURI", "route/passenger/listActive.do");
-
-		return result;
-	}
+	// Creation ---------------------------------------------------------------
 
 	@RequestMapping(value = "/display", method = RequestMethod.GET)
 	public ModelAndView display(@RequestParam final int routeID) {
@@ -140,88 +97,6 @@ public class RouteController extends AbstractController {
 		return result;
 	}
 
-	// Creation ---------------------------------------------------------------
-	@RequestMapping(value = "/driver/create", method = RequestMethod.GET)
-	public ModelAndView create() {
-		ModelAndView result;
-		Route route;
-
-		route = this.routeService.create();
-
-		result = this.createEditModelAndView(route);
-
-		return result;
-	}
-
-	@RequestMapping(value = "driver/edit", method = RequestMethod.GET)
-	public ModelAndView edit(@RequestParam final int routeId) {
-		final ModelAndView result;
-		Route route;
-
-		route = this.routeService.findOne(routeId);
-		Assert.notNull(route);
-		result = this.createEditModelAndView(route);
-
-		return result;
-	}
-
-	@RequestMapping(value = "/driver/edit", method = RequestMethod.POST, params = "save")
-	public ModelAndView save(@Valid final Route route, final BindingResult binding) {
-		Route saved;
-
-		if (route.getId() != 0)
-			Assert.isTrue(route.getDriver().getId() == this.actorService.findByPrincipal().getId());
-
-		ModelAndView result;
-		for (final ObjectError oe : binding.getAllErrors())
-			System.out.println(oe);
-		if (binding.hasErrors()) {
-			System.out.println(binding.getAllErrors());
-			result = this.createEditModelAndView(route);
-		} else
-			try {
-				saved = this.routeService.save(route);
-				result = new ModelAndView("redirect:/route/driver/confirmRoute.do?routeId=" + saved.getId());
-			} catch (final Throwable oops) {
-				oops.printStackTrace();
-				result = this.createEditModelAndView(route, "driver.commit.error");
-			}
-		return result;
-	}
-
-	@RequestMapping(value = "driver/delete", method = RequestMethod.GET)
-	public ModelAndView delete(final Route route, final BindingResult binding) {
-		ModelAndView result;
-
-		try {
-			this.routeService.delete(route);
-			result = new ModelAndView("redirect:create.do");
-		} catch (final Throwable oops) {
-			result = this.createEditModelAndView(route, "route.commit.error");
-		}
-		return result;
-	}
-
-	@RequestMapping(value = "/driver/confirmRoute", method = RequestMethod.GET)
-	public ModelAndView confirmRoute(@RequestParam final int routeId) {
-		ModelAndView result;
-		Route route;
-		final Driver driver;
-		UserAccount ua;
-
-		ua = LoginService.getPrincipal();
-		driver = (Driver) this.actorService.findByUserAccount(ua);
-		Assert.notNull(driver);
-
-		route = this.routeService.findOne(routeId);
-
-		result = new ModelAndView("route/driver/confirmRoute");
-		result.addObject("route", route);
-		result.addObject("requestURI", "route/driver/confirmRoute.do");
-
-		return result;
-	}
-
 	// Ancilliary methods ----------------------------------------------------------------
 	private ModelAndView createEditModelAndView(final Route route) {
 		ModelAndView result;
@@ -241,6 +116,19 @@ public class RouteController extends AbstractController {
 		result = new ModelAndView("route/driver/create");
 		result.addObject("route", route);
 		result.addObject("vehicles", driver.getVehicles());
+		result.addObject("message", message);
+		result.addObject("requestURI", requestURI);
+
+		return result;
+	}
+
+	private ModelAndView routeDisplayModelAndView(final Route route, final String message) {
+		ModelAndView result;
+		String requestURI;
+
+		requestURI = "route/display.do";
+		result = new ModelAndView("route/display");
+		result.addObject("route", route);
 		result.addObject("message", message);
 		result.addObject("requestURI", requestURI);
 
